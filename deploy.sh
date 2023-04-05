e3a55b602986db27a2b8917860115c60005da391
Vikram Pyati <vipyati@ebay.com>
2023-04-04 17:29:46 -0700

    cloud deployment

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+gcloud auth login
+gcloud config set project ebay-summary
+cd ebaysummary
+gcloud functions deploy ebaysummary-scheduled
--- googlecloud/main.py
+++ googlecloud/main.py
@@ -38,7 +38,7 @@ def send_email(to,subject,content):
 
 def get_posts_and_replies(subreddit):
     posts_and_replies = []
-    for submission in subreddit.new(limit=3):
+    for submission in subreddit.new(limit=5):
         post = {'title': submission.title, 'text': submission.selftext, 'replies': []}
         posts_and_replies.append(post)
 
@@ -54,7 +54,7 @@ def summarize_posts(posts_and_replies):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a assitant which can summarize user's post on eBay subreddit"},
-            {"role": "user", "content": f"The following sentences are from the eBay subreddit where ebay users discuss their issues. Summarize the texts and present few important issues that the users are talking about and give some hastags as well. Also, check if there are any common themes across posts: {input_text}"}
+            {"role": "user", "content": f"The following sentences are from the eBay subreddit where ebay users discuss their issues. Summarize the texts and present few important issues that the users are talking about and give some hastags as well: {input_text}"}
         ]
     )
 
