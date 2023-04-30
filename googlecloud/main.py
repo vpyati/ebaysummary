cae92bccd1083e5db6d742073db590dede97cf18
Vikram Pyati <vipyati@ebay.com>
2023-04-29 21:57:10 -0700

    change prompt

--- googlecloud/main.py
+++ googlecloud/main.py
@@ -4,6 +4,7 @@ import openai
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+
 def getSecret(name):
     return os.environ.get(name, 'Specified environment variable is not set.')
 
@@ -13,11 +14,11 @@ openai.api_key = getSecret("openai_api_key")
 
 # Initialize the Reddit instance
 user_agent = "ebaysummary 1.0 by /u/yhsls github.com/vpyati/ebaysummary"
-reddit = praw.Reddit(client_id=getSecret("reddit_client_id"), client_secret=getSecret("reddit_client_secret"), user_agent=user_agent)
+reddit = praw.Reddit(client_id=getSecret("reddit_client_id"), client_secret=getSecret("reddit_client_secret"),
+                     user_agent=user_agent)
 
 
-
-def send_email(to,subject,content):
+def send_email(to, subject, content):
     sendgrid_api_key = getSecret("sendgrid_api_key")
     sender_email = 'vikrampyati@gmail.com'
 
@@ -44,30 +45,38 @@ def get_posts_and_replies(subreddit):
 
     return posts_and_replies
 
+
 def summarize_posts(posts_and_replies) -> str:
     input_texts = []
     for post in posts_and_replies:
-        input_texts.append('<p>'+post['title'] + ". " + post['text']+'<p>')
+        input_texts.append('<p>' + post['title'] + ". " + post['text'] + '<p>')
 
     input_text = '.'.join(input_texts)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=0,
         messages=[
-            {"role": "system", "content": "You are a assitant which can summarize user's post on eBay subreddit and classify them in different topics"},
-            {"role": "user", "content": f"The following posts are from the eBay subreddit where ebay users discuss their issues.Each post is between tags <p>.Summarize each post in a separate bullet point and add some hastags as well. For each post, determine if it is a issue or a question. If it is an issue output 'Issue' as the last hashtag, else output 'Question' : {input_text}"}
+            {"role": "system",
+             "content": "You are a assitant which can summarize user's post on eBay subreddit and classify them in different topics"},
+            {"role": "user",
+             "content": f"The following posts are from the eBay subreddit where ebay users discuss their issues."
+                        f"Each post is between tags <p>.Create a HTML table with border with following information as different column with a row for each post "
+                        f"Summary of the post in at most 50 words, Hashtags describing the post, determine if it is a issue or a question. If it is an issue output 'Issue', else output 'Question'"
+                        f": {input_text}"}
         ]
     )
 
-    return response.choices[0].message.content.strip().replace("\n","<br>")
+    return response.choices[0].message.content.strip()
+
 
 def summaries(data, context):
     subreddit = reddit.subreddit("Ebay")
     posts_and_replies = get_posts_and_replies(subreddit)
     summaries_data = summarize_posts(posts_and_replies)
     summaries_data = "The summary of some recent posts on Ebay subreddit is as below <br><br>" + summaries_data
-    send_email('vikrampyati@gmail.com','Ebay reddit summary',summaries_data)
+    send_email('vikrampyati@gmail.com', 'Ebay reddit summary', summaries_data)
     return summaries_data
 
+
 if __name__ == "__main__":
-    print(summaries("",""))
+    print(summaries("", ""))
